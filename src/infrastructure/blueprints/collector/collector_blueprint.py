d561eaf4e4786435fcdfc31efc357cc8e92e82ff
AdrianM0Hdz <adrian.hernandez.p0@gmail.com>
2023-09-07 16:40:38 -0600

    added collector endpoint in blueprint

--- src/infrastructure/blueprints/collector/collector_blueprint.py
+++ src/infrastructure/blueprints/collector/collector_blueprint.py
@@ -1,7 +1,8 @@
-from flask import Blueprint, Response, request, jsonify
+from flask import Blueprint, request, jsonify
 
 from src.application.read_model.collector import CollectorData
 from src.infrastructure.persistence.queries.collector.get_collector_by_id import get_collector_by_id
+from src.infrastructure.blueprints.serializers.collector import serialize_collector_to_json
 
 ticket_blueprint = Blueprint('ticket_blueprint', __name__)
 
@@ -9,4 +10,5 @@ ticket_blueprint = Blueprint('ticket_blueprint', __name__)
 def get_collector_by_id_handle(id: str):
     assert isinstance(id, str)
     collector: CollectorData = get_collector_by_id(id=id)
-    return jsonify()
+    collector_json: dict = serialize_collector_to_json(collector)
+    return jsonify(**collector_json)
