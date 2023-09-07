ed059545df02829aef6891267cf04c78fd45f5ce
AdrianM0Hdz <adrian.hernandez.p0@gmail.com>
2023-09-07 16:03:55 -0600

    aded basic branch skeleton

--- src/infrastructure/persistence/blueprints/ticket/ticket_blueprint.py
+++ src/infrastructure/persistence/blueprints/ticket/ticket_blueprint.py
@@ -1,8 +1,9 @@
 from flask import Blueprint, Response, request, jsonify
 
+from src.infrastructure.persistence.queries.ticket.get_ticket_by_id import get_ticket_by_id
+
 ticket_blueprint = Blueprint('ticket_blueprint', __name__)
 
 @ticket_blueprint.route("/<string:id>", methods=["GET"])
 def get_ticket_by_id_handler(id: str):
-    
     return jsonify()
