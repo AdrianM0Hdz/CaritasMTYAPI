92223411f81a1e3823a7e8de7eec7c18a17aec58
JaimedevSwirvle <jaime@swirvle.com>
2023-09-07 16:37:54 -0600

    Terminados getters de manager

--- src/infrastructure/persistence/queries/manager/get_manager_by_username.py
+++ src/infrastructure/persistence/queries/manager/get_manager_by_username.py
@@ -2,12 +2,14 @@ import os
 
 import pyodbc
 
-from src.application.read_model.collector import CollectorData
+from src.application.read_model.manager import ManagerData
 
-from src.infrastructure.persistence.queries.ticket.get_tickets_by_collector_id import get_tickets_by_collector_id
-from src.infrastructure.persistence.queries.serializers.collector.deserialize_collector import deserialize_collector
+from src.infrastructure.persistence.queries.ticket.get_tickets_by_manager_id import get_tickets_by_manager_id
+from src.infrastructure.persistence.queries.collector.get_collectors_by_manager_id import get_collectors_by_manager_id
 
-def get_collector_by_username(username: str) -> CollectorData:
+from src.infrastructure.persistence.queries.serializers.manager.deserialize_manager import deserialize_manager
+
+def get_manager_by_username(username: str) -> ManagerData:
     connectionstring = f'DRIVER={{ODBC Driver 17 for SQL Server}}; \
                              SERVER={os.environ["SERVER"]}; \
                              DATABASE={os.environ["DATABASE"]}; \
@@ -16,20 +18,22 @@ def get_collector_by_username(username: str) -> CollectorData:
     conn = pyodbc.connect(connectionstring)
     cursor = conn.cursor()
 
-    query = f"SELECT * FROM Collector WHERE username='{username}'"
+    query = f"SELECT * FROM Manager WHERE Username='{username}'"
     cursor.execute(query)
     
     data = cursor.fetchall() 
     
     if len(data) == 0:
-        raise BaseException("NO COLLECTOR WITH THAT USERNAME FOUND")
+        raise BaseException("There's no manager with that Username")
 
     if len(data) > 1:
-        raise BaseException("MORE THAN ONE COLLECTOR WITH THAT USERNAME")
+        raise BaseException("More than one item fetched, theres several managers with the same username")
      
-    collector_raw_data = data[0]
+    manager_raw_data = data[0]
+    managerID = manager_raw_data[0]
 
-    tickets = get_tickets_by_collector_id(collector_raw_data[0])
+    tickets = get_tickets_by_manager_id(managerID)
+    collectors = get_collectors_by_manager_id(managerID)
 
-    return deserialize_collector(collector_raw_data, list(tickets))
+    return deserialize_manager(manager_raw_data, list(tickets), list(collectors))
 
