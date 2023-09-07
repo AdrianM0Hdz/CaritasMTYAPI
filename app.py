e5ef92fc705b7e0361a756ef0561bc1eabd875bc
AdrianM0Hdz <adrian.hernandez.p0@gmail.com>
2023-09-07 17:23:36 -0600

    added collector endpoints for query via id and login

--- app.py
+++ app.py
@@ -2,13 +2,9 @@ import os
 from dotenv import load_dotenv
 load_dotenv(override=True)
 
-from src.infrastructure.persistence.manager_repo import ManagerRepository
-from src.domain.manager import Manager
+from src.infrastructure.create_app import create_app
 
-from src.infrastructure.persistence.queries.ticket import get_ticket_by_id
-from src.infrastructure.persistence.queries.ticket.get_tickets_by_collector_id import get_tickets_by_collector_id
-from src.infrastructure.persistence.queries.collector.get_collector_by_id import get_collector_by_id
-from src.infrastructure.persistence.queries.collector.get_collector_by_username import get_collector_by_username
-from src.infrastructure.persistence.queries.collector.get_collectors_by_manager_id import get_collectors_by_manager_id
+app = create_app()
 
-print(get_collectors_by_manager_id(manager_id="1"))
+# TODO: this is provitional and for development
+app.run(host="0.0.0.0", debug=True)
