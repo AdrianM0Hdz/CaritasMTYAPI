4feace769136f2c4e205139b43c7ebaa595a5dbf
JaimedevSwirvle <jaime@swirvle.com>
2023-09-22 11:22:32 -0600

    Ya jaló login

--- src/infrastructure/blueprints/general/general_blueprint.py
+++ src/infrastructure/blueprints/general/general_blueprint.py
@@ -1,22 +1,17 @@
 from flask import Blueprint, request, jsonify
 
 from src.application.read_model.collector import CollectorData
+from src.application.read_model.manager import ManagerData
+
 from src.application.queries.collector import login_collector
+from src.application.queries.manager import login_manager
 
-from src.infrastructure.persistence.queries.collector.get_collector_by_id import get_collector_by_id
-from src.infrastructure.blueprints.serializers.collector import serialize_collector_to_json
 
-collector_blueprint = Blueprint('collector_blueprint', __name__)
 
-@collector_blueprint.route("/<string:id>", methods=["GET"])
-def get_collector_by_id_handle(id: int):
-    assert isinstance(id, int)
-    collector: CollectorData = get_collector_by_id(id=id)
-    collector_json: dict = serialize_collector_to_json(collector)
-    return jsonify(**collector_json)
+general_blueprint = Blueprint('general_blueprint', __name__)
 
-@collector_blueprint.route("/login", methods=["POST"])
-def login_collector_handle():
+@general_blueprint.route("/login", methods=["POST"])
+def login_handle():
     data = request.get_json()
     if not data:
         print("no json body")
@@ -34,9 +29,27 @@ def login_collector_handle():
             msg="username and password parameters must be given"
         ), 400
     print("passed check")
+
     try:
-        collector: CollectorData  = login_collector(username=username, password=password)
-        collector_json = serialize_collector_to_json(collector)
-        return jsonify(id=collector_json["id"])
+
+        try: 
+            collector: CollectorData  = login_collector(username=username, password=password)
+        except BaseException as inst:
+            print(inst)
+            collector = None
+        
+        try:
+            manager: ManagerData  = login_manager(username=username, password=password)
+        except BaseException as inst:
+            print(inst)
+            manager = None
+
+        if collector == None and manager == None:
+            raise BaseException("INCORRECT USERNAME OR PASSWORD")
+        elif collector == None:
+            return jsonify(id=manager.id, role="MANAGER")
+        else:
+            return jsonify(id=collector.id, role="COLLECTOR")
+        
     except BaseException as inst:
         return jsonify(msg=str(inst)), 400
