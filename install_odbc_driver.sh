06f2e72f453473bab5ea309acaf69557559e83e2
AdrianM0Hdz <adrian.hernandez.p0@gmail.com>
2023-10-04 18:52:48 -0600

    asdf

--- install_odbc_driver.sh
+++ install_odbc_driver.sh
@@ -1,4 +1,6 @@
-if ! [[ "18.04 20.04 22.04 23.04" == *"$(lsb_release -rs)"* ]];
+#!/bin/bash
+
+if ![[ "18.04 20.04 22.04 23.04" == *"$(lsb_release -rs)"* ]];
 then
     echo "Ubuntu $(lsb_release -rs) is not currently supported.";
     exit;
@@ -18,5 +20,6 @@ export ACCEPT_EULA=Y
 apt-get install -y mssql-tools18
 echo 'export PATH="$PATH:/opt/mssql-tools18/bin"' >> ~/.bashrc
 . ~/.bashrc
+
 # optional: for unixODBC development headers
 apt-get install -y unixodbc-dev
