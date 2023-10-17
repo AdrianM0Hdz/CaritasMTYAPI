6275303df39607ad028ddf72a7b125b18a66fe02
AdrianM0Hdz <adrian.hernandez.p0@gmail.com>
2023-10-17 07:25:41 -0600

    added logs to manager_blueprint

--- src/infrastructure/blueprints/manager/manager_blueprint.py
+++ src/infrastructure/blueprints/manager/manager_blueprint.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, request, jsonify
 
+from src.utils.logger import logger
+
 from src.application.read_model.manager import ManagerData
 from src.application.queries.manager import login_manager
 
@@ -10,15 +12,21 @@ manager_blueprint = Blueprint('manager_blueprint', __name__)
 
 @manager_blueprint.route("/<string:id>", methods=["GET"])
 def get_manager_by_id_handle(id: int):
-    assert isinstance(id, str)
-    manager: ManagerData = get_manager_by_id(id=id)
-    manager_json: dict = serialize_manager_to_json(manager)
-    return jsonify(**manager_json)
+    try: 
+        assert isinstance(id, str)
+        manager: ManagerData = get_manager_by_id(id=id)
+        manager_json: dict = serialize_manager_to_json(manager)
+        logger.info(f"MANAGER WITH ID: {id} FETCHED BY {request.remote_addr}")
+        return jsonify(**manager_json)
+    except BaseException as inst:
+        logger.critical(f"FAILED TO FETCH MANAGER WITH ID: {id} BY {request.remote_addr}")
+        return jsonify(msg="ERROR"),400
 
 @manager_blueprint.route("/login", methods=["POST"])
 def login_manager_handle():
     data = request.get_json()
     if not data:
+        logger.error(f"NO REQUEST BODY GIVEN BY {request.remote_addr} TO LOG IN MANAGER")
         return jsonify(
             msg="just must give username and password"
         ), 400
@@ -28,6 +36,7 @@ def login_manager_handle():
         username=data["username"]
         password=data["password"]
     except KeyError:
+        logger.error(f"NO VAILD PRAMETERS GIVEN IN JSON BODY BY {request.remote_addr}, TO LOG IN MANAGER")
         return jsonify(
             msg="username and password parameters must be given"
         ), 400
@@ -35,7 +44,8 @@ def login_manager_handle():
     try:
         manager: ManagerData  = login_manager(username=username, password=password)
         manager_json = serialize_manager_to_json(manager)
+        logger.info(f"MANAGER WITH USERNAME: {username} LOGGED IN BY {request.remote_addr}")
         return jsonify(id=manager_json["id"])
     except BaseException as inst:
+        logger.critical(f"FAILED TO LOG IN MANAGER: {username} BY {request.remote_addr}", exc_info=True)
         return jsonify(msg=str(inst)), 400
-        
